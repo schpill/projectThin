ba35a34caf13c61105175b9ce83ba50a1f07460e
Gerald P <gplusquellec@crakmedia.com>
2014-01-10 16:19:42 -0500

    fonctions JS

--- web/assets/js/web.js
+++ web/assets/js/web.js
@@ -200,3 +200,237 @@ webJS.connect = function(lng) {
         }
     }
 };
+
+var thin = (function () {
+    var self = this;
+    _baseKeyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    this._thin = {};
+
+    // public method for encoding
+    this.encode = function (input) {
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = this._utf8_encode(input);
+
+        while (i < input.length) {
+
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+
+            output = output +
+            _baseKeyStr.charAt(enc1) + _baseKeyStr.charAt(enc2) +
+            _baseKeyStr.charAt(enc3) + _baseKeyStr.charAt(enc4);
+
+        }
+
+        return output;
+    }
+
+    // public method for decoding
+    this.decode = function (input) {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+        while (i < input.length) {
+            enc1 = _baseKeyStr.indexOf(input.charAt(i++));
+            enc2 = _baseKeyStr.indexOf(input.charAt(i++));
+            enc3 = _baseKeyStr.indexOf(input.charAt(i++));
+            enc4 = _baseKeyStr.indexOf(input.charAt(i++));
+
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+
+            output = output + String.fromCharCode(chr1);
+            if (enc3 != 64) {
+                output = output + String.fromCharCode(chr2);
+            }
+            if (enc4 != 64) {
+                output = output + String.fromCharCode(chr3);
+            }
+        }
+        output = this._utf8_decode(output);
+        return output;
+    }
+
+    // private method for UTF-8 encoding
+    this._utf8_encode = function (string) {
+        string = string.replace(/\r\n/g,"\n");
+        var utftext = "";
+
+        for (var n = 0; n < string.length; n++) {
+
+            var c = string.charCodeAt(n);
+
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+
+        }
+
+        return utftext;
+    }
+
+    // private method for UTF-8 decoding
+    this._utf8_decode = function (utftext) {
+        var string = "";
+        var i = 0;
+        var c = c1 = c2 = 0;
+
+        while ( i < utftext.length ) {
+
+            c = utftext.charCodeAt(i);
+
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            }
+            else if((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i+1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            }
+            else {
+                c2 = utftext.charCodeAt(i+1);
+                c3 = utftext.charCodeAt(i+2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+
+        }
+
+        return string;
+    }
+
+    this.localCache = function(name, value) {
+        try {
+            if (window.localStorage) {
+                if (typeof(value) != "undefined") {
+                    localStorage.setItem(name, value);
+                } else {
+                    return localStorage.getItem(name);
+                }
+            }
+        } catch (err) {
+        }
+    }
+
+    this.sessionCache = function(name, value) {
+        try {
+            if (window.sessionStorage) {
+                if (typeof(value) != "undefined") {
+                    sessionStorage.setItem(name, value);
+                } else {
+                    return sessionStorage.getItem(name);
+                }
+            }
+        } catch(err) {
+        }
+    }
+
+    this.globalCache = function(name, value) {
+        if (window.globalStorage) {
+            var host = this.getHost();
+            try  {
+                if (typeof(value) != "undefined") {
+                    eval("globalStorage[host]." + name + " = value");
+                } else {
+                    return eval("globalStorage[host]." + name);
+                }
+            } catch(err)  {
+            }
+        }
+    }
+    this.evercookie_database_storage = function(name, value) {
+        try {
+            if (window.openDatabase) {
+                var database = window.openDatabase("db_thin", "", "thin", 1024 * 1024);
+                if (typeof(value) != "undefined") {
+                    database.transaction(function(tx) {
+                            tx.executeSql("CREATE TABLE IF NOT EXISTS cache(" +
+                                "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                                "name TEXT NOT NULL, " +
+                                "value TEXT NOT NULL, " +
+                                "UNIQUE (name)" +
+                            ")", [], function (tx, rs) { }, function (tx, err) { }
+                        );
+                        tx.executeSql("INSERT OR REPLACE INTO cache(name, value) VALUES(?, ?)", [name, value],
+                            function (tx, rs) { }, function (tx, err) { })
+                    });
+                } else {
+                    database.transaction(function(tx) {
+                        tx.executeSql("SELECT value FROM cache WHERE name=?", [name],
+                            function(tx, result1) {
+                                if (result1.rows.length >= 1) {
+                                    self._thin.dbData = result1.rows.item(0)['value'];
+                                } else {
+                                    self._thin.dbData = '';
+                                }
+                            }, function (tx, err) { }
+                        )
+                    });
+                }
+            }
+        } catch(err) { }
+    }
+
+    this.getHost = function() {
+        var domain = document.location.host;
+        if (domain.indexOf('www.') == 0) {
+            domain = domain.replace('www.', '');
+        }
+        return domain;
+    }
+
+    this.createElem = function(type, name, append) {
+        var el;
+        if (typeof name != 'undefined' && document.getElementById(name)) {
+            el = document.getElementById(name);
+        } else {
+            el = document.createElement(type);
+        }
+
+        if (name) {
+            el.setAttribute('id', name);
+        }
+
+        if (append) {
+            document.body.appendChild(el);
+        }
+
+        return el;
+    }
+
+    this.createIframe = function(url, name) {
+        var el = this.createElem('iframe', name, 1);
+        el.setAttribute('src', url);
+        return el;
+    }
+});
