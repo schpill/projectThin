75ae42e9a3a158a0a94c8f92ce6d6f283f10acf8
Gerald P <gplusquellec@crakmedia.com>
2014-01-10 09:00:49 -0500

    storage + vendor

---
+++ storage/translation/menu/uk.php
@@ -0,0 +1,12 @@
+<?php
+    $sentences = array(
+        'about' => 'About',
+        'vendors' => 'Our vendors',
+        'contact' => 'Contact',
+        'register' => 'Sign in',
+        'Become member' => 'Become member',
+        'account' => 'My account',
+        'logout' => 'Logout',
+    );
+
+    return $sentences;
